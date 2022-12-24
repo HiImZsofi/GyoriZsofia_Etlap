bfb0870b96c963876210064b492adc722c7c416b
Zsófia Győri <zsofia.gyori00@gmail.com>
2022-12-24 11:52:35 +0100

    meall-view-form

--- src/main/java/com/example/gyorizsofia_etlap/AppController.java
+++ src/main/java/com/example/gyorizsofia_etlap/AppController.java
@@ -3,7 +3,7 @@ package com.example.gyorizsofia_etlap;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class AppController {
     @FXML
     private Label welcomeText;
 
